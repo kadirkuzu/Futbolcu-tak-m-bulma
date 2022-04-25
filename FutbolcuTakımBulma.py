cc254d9401af93c70a1839ff93359bccfea45b55
kadirkuzu <kadirkuzu.5656@gmail.com>
2022-04-25 17:22:44 +0300

    first commit

--- FutbolcuTakımBulma.py
+++ FutbolcuTakımBulma.py
@@ -1,3 +1,6 @@
+open("Galatasaray.txt",'w',encoding='UTF-8')
+open("Fenerbahce.txt",'w',encoding='UTF-8')
+open("Besiktas.txt",'w',encoding='UTF-8')
 with open("futbolcu.txt",'r' , encoding='UTF-8') as futbolcular :
     fenerbahceliler = []
     besiktaslılar = []
@@ -5,18 +8,18 @@ with open("futbolcu.txt",'r' , encoding='UTF-8') as futbolcular :
     okuma = futbolcular.readlines()
     for i in okuma :
         default = i.split(',')
-        if default[1]=='Fenerbahçe\n':
+        if default[1]=='Fenerbahçe\n'or default[1]=='Fenerbahçe':
             fenerbahceliler.append(default[0])
-        elif default[1]=='Galatasaray\n':
+        elif default[1]=='Galatasaray\n'or default[1]=='Galatasaray':
             galatasaraylılar.append(default[0])
-        elif default[1]=='Beşiktaş\n':
+        elif default[1]=='Beşiktaş\n'or default[1]=='Beşiktaş\n':
             besiktaslılar.append(default[0])
         else :
             print('hata oluştu')
     print(fenerbahceliler)
     print(galatasaraylılar)
     print(besiktaslılar)
-with open("besiktas.txt" , 'a',encoding='UTF-8')as besiktas:
+with open("Besiktas.txt", 'a', encoding='UTF-8')as besiktas:
     for i in besiktaslılar :
         besiktas.write(i+'\n')
 with open("Fenerbahce.txt",'a',encoding='UTF-8')as fenerbahce :
